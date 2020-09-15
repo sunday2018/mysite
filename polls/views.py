a469c62f73723e525c9959d313b55418b1c25601
sunday2018 <chige-hua@163.com>
2020-09-15 15:16:40 +0800

    commit

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 # Create your views here.
@@ -33,7 +33,7 @@ def vote(request, question_id):
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except (KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/detail.html', {'question':question, 'error_message':"You didn't select a choice."})
+		return render(request, 'polls/detail.html', {'question':question, 'error_message':"You didn't select a choice."},)
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
